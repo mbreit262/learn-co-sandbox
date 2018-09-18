0ced775ad09c8f25016e7f3caa12f3f2a80af200
Matthew Breit <mbreit@layerframe.com>
2018-09-18 21:49:07 +0000

    automatically backed up by learn

---
+++ datatypes.py
@@ -0,0 +1,10 @@
+# 1. Grab the number 6 from this nested_list and set it equal to the number_six variable
+
+nested_list = [1, [2, [4, [5, [6]], 3]]]
+number_six = None
+
+# 2. Sort the cats list in alphabetical order
+
+cats = ['Maine Coon', 'Tabby', 'Siamese', 'Garfield', 'Sylvester']
+
+# 3. Look up a new string or list method and explain how it works
